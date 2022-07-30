f1a09a5e763974825158ef9d430699f9691573b5
phamthang11782 <phamthang11782@gmail.com>
2022-07-30 09:58:18 +0700

    add data and setup libs

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{js,jsx}'],
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+     extend: {
+      fontFamily: {
+        rnroll: ['RocknRoll One', 'sans-serif']
+      }
+     },
+     fontFamily: {
+      sans: ['Istok Web', 'sans-serif']
+     }
+  },
+  plugins: [],
+};
